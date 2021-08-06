0185fbef42fced934ae2309d908052c21a02ee39
David Xie <david30xie@gmail.com>
2021-08-06 21:48:13 +0100

    Update print_love_picture.py

--- python/lesson13/print_love_picture.py
+++ python/lesson13/print_love_picture.py
@@ -1,6 +1,36 @@
-#-*-coding: utf-8-*-
 import turtle
 import time
+
+
+def draw_setup():
+    turtle.Screen().setup(width=900, height=600)
+    turtle.color('red') # 设置颜色, 可以改成你喜欢的颜色
+    turtle.pensize(15) # 设置笔墨的粗细
+    turtle.speed(10)
+    turtle.up()
+    turtle.goto(0,-180)
+    turtle.down()
+
+
+def draw_little_heart():
+    for i in range(200):
+        turtle.right(1)
+        turtle.forward(2)
+
+
+def draw_left():
+    turtle.fillcolor("pink")
+    turtle.begin_fill()
+    turtle.left(140)
+    turtle.forward(224)
+    draw_little_heart()
+
+
+def draw_right():
+    turtle.left(120)
+    draw_little_heart()
+    turtle.forward(224)
+    turtle.end_fill()
 
 
 def write(start, end):
@@ -10,50 +40,8 @@ def write(start, end):
     turtle.goto(end)
 
 
-def draw_little_heart():
-    for i in range(200):
-        turtle.right(1)
-        turtle.forward(2)
-
-
-def draw_setup():
-    #turtle.color('red', 'pink')
-    turtle.pensize(15)
-    #turtle.speed(2000)
-
-    turtle.up()
-
-
-def draw_left():
-    turtle.hideturtle()
-    turtle.color('red')
-    turtle.goto(0,-180)
-    turtle.showturtle()
-    turtle.down()
-    turtle.speed(2000)
-    turtle.begin_fill()
-    turtle.color('black')
-    turtle.left(140)
-    turtle.forward(224)
-    draw_little_heart()
-
-
-def draw_right():
-    turtle.color('black')
-    turtle.left(120)
-
-    draw_little_heart()
-
-    turtle.forward(224)
-    turtle.end_fill()
-    turtle.pensize(12)
-    turtle.up()
-    turtle.hideturtle()
-    turtle.goto(0, -20)
-    turtle.showturtle()
-
-
 def draw_words():
+    turtle.pensize(15)
     turtle.color('red')
     # sheng
     write((-150, 30), (-160, 10))
@@ -65,51 +53,42 @@ def draw_words():
     # ri
     write((-75, 35), (-75, -35))
     write((-75, 35), (-20, 35))
-    turtle.goto((-20,-35))
+    write((-20, 35), (-20,-35))
     write((-75, 0), (-20, 0))
     write((-75, -35), (-20, -35))
-
 
     # kuai
     write((30, 30), (20, 0))
     write((30, 30), (40, 0))
     write((30, 40), (30, -40))
     write((50, 20), (80, 20))
-    turtle.goto((80, 0))
+    write((80, 20), (80, 0))
     write((35, 0), (100, 0))
     write((65, 40), (65, 0))
-    turtle.goto((40, -40))
+    write((65, 0), (40, -40))
     write((65, 40), (65, 0))
     write((65, 0), (80, -40))
 
     # le
     write((160, 40), (110, 30))
-    turtle.goto((110, 10))
-    turtle.goto((170, 10))
+    write((110, 30), (110, 10))
+    write((110, 10), (170, 10))
     write((140, 35), (140, -40))
-    turtle.goto((130, -30))
+    write((140, -40), (130, -30))
     write((120, -10), (110, -30))
     write((160, -10), (170, -30))
     turtle.up()
 
-
 def draw_person(person):
-    turtle.hideturtle()
-    turtle.color('red')
+    turtle.color('black') # 改一下颜色
     time.sleep(1)
     turtle.goto(180, -180)
-    turtle.showturtle()
-    turtle.write(person, font=(20, 25), align="right", move=True)
-
-
-person = input('送给谁: ')
-
+    turtle.write(person, font=(20, 25), align="center", move=True)
 
 draw_setup()
 draw_left()
 draw_right()
 draw_words()
-draw_person(person)
 
-window = turtle.Screen()
-window.exitonclick()
+person = input('送给谁: ')
+draw_person("person")
